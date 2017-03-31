91690eb6e48ac7bbed0bd9598deab7d81ff6b436
Robert Murray <work@robertmurray.me>
2017-03-31 13:01:39 -0500

    updated the connection string

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
 
   development: {
     client: "pg",
-    connection: "postgres://wecsizmqdydkfs:7b965772a6a619d0e60a1d530d73295588b991ebecf6f6a467d40735a55f84d1@ec2-54-221-244-196.compute-1.amazonaws.com:5432/dfh21bdlmk6j0g",
+    connection: "postgres://wecsizmqdydkfs:7b965772a6a619d0e60a1d530d73295588b991ebecf6f6a467d40735a55f84d1@ec2-54-221-244-196.compute-1.amazonaws.com:5432/dfh21bdlmk6j0g?ssl=true",
     migrations: {
       directory: `${__dirname}/db/migrations`
     },
